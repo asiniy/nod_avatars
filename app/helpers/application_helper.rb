ebe42947ec24e92e2182e9ff7274969eff773af0
Alex Antonov <kaburbundokel11g@inbox.ru>
2014-02-15 21:37:39 +0400

    social networks icons & links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@ module ApplicationHelper
   def social_network_url(photo)
     case photo.provider_name
     when 'vk' then "http://vk.com/id#{photo.provider_uid}"
+    when 'fb' then "http://facebook.com/profile.php?id=#{photo.provider_uid}"
+    when 'ok' then "http://odnoklassniki.ru/profile/#{photo.provider_uid}"
     else raise 'no such provider'
     end
   end
@@ -9,6 +11,8 @@ module ApplicationHelper
   def social_network_icon(photo)
     case photo.provider_name
     when 'vk' then image_tag('vk_icon.png')
+    when 'fb' then image_tag('fb_icon.gif')
+    when 'ok' then image_tag('ok_icon.png')
     else raise 'no such provider'
     end
   end
